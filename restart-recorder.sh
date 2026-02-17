fc301cc2672dfd1211ab212c2ccac382f806fafa
Warren Downs <vwdowns@gmail.com>
2026-02-16 20:59:11 -0900

    Safe recorder restart

--- restart-recorder.sh
+++ restart-recorder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 s=$(echo "3600-($(date +%s)%3600)-3" | bc)
-echo "Sleeping $s until top of hour recorder restart"
+echo "Sleeping $s seconds until top of hour recorder restart"
 sleep $s
 systemctl restart --user threeabn-recorder
 journalctl --user -u threeabn-recorder -f
