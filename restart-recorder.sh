809bde1eac5cf305b0123519c8b14d2be73b5954
Warren Downs <vwdowns@gmail.com>
2026-02-16 21:12:52 -0900

    rec lead in fix; play error catch; restart rec safely

--- restart-recorder.sh
+++ restart-recorder.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-s=$(echo "3600-($(date +%s)%3600)-3" | bc)
-echo "Sleeping $s seconds until top of hour recorder restart"
-sleep $s
+rec=$(ps waxf | grep -v grep | grep mpv,*dump)
+if [ "$rec" != "" ]; then
+	s=$(echo "3600-($(date +%s)%3600)-3" | bc)
+	echo "Sleeping $s seconds until top of hour recorder restart"
+	sleep $s
+else
+	echo "No recording in process.  Restarting now..."
+fi
 systemctl restart --user threeabn-recorder
 journalctl --user -u threeabn-recorder -f
