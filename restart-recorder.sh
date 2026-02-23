2844391c3e2c74f369f4e241ffcf1fadfb902fb3
Warren Downs <warren@choggiung.com>
2026-02-23 08:56:01 -0900

    Fix typo in restart-recorder

--- restart-recorder.sh
+++ restart-recorder.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rec=$(ps waxf | grep -v grep | grep mpv,*dump)
+rec=$(ps waxf | grep -v grep | grep mpv.*dump)
 if [ "$rec" != "" ]; then
 	s=$(echo "3600-($(date +%s)%3600)-3" | bc)
 	echo "Sleeping $s seconds until top of hour recorder restart"
